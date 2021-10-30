d7496855f5c54aedeb0bdf5a958a11e57aa965f3
Timo Sutterer <hi@timo-sutterer.de>
2021-10-30 20:52:42 +0200

    feat: add constants for feature and target names

--- src/nntm/datasets/__init__.py
+++ src/nntm/datasets/__init__.py
@@ -8,6 +8,16 @@ from ._numerai_main import (
     fetch_numerai_example_validation_predictions,
     fetch_numerai_feature_metadata,
 )
+from ._numerai_main_meta import (
+    TARGET_NAMES_UNIQUE,
+    TARGET_NAMES,
+    FEATURE_NAMES_LEGACY,
+    FEATURE_NAMES_SMALL,
+    FEATURE_NAMES_MEDIUM,
+    COLUMN_NAMES_LEGACY,
+    COLUMN_NAMES_SMALL,
+    COLUMN_NAMES_MEDIUM,
+)
 
 __all__ = [
     "fetch_numerai_training",
@@ -18,4 +28,12 @@ __all__ = [
     "fetch_numerai_example_predictions",
     "fetch_numerai_example_validation_predictions",
     "fetch_numerai_feature_metadata",
+    "TARGET_NAMES_UNIQUE",
+    "TARGET_NAMES",
+    "FEATURE_NAMES_LEGACY",
+    "FEATURE_NAMES_SMALL",
+    "FEATURE_NAMES_MEDIUM",
+    "COLUMN_NAMES_LEGACY",
+    "COLUMN_NAMES_SMALL",
+    "COLUMN_NAMES_MEDIUM",
 ]
